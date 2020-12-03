bd1084bbad2f89f4b9a6f0b8bd6ea637df06b91c
Llelepipede <pchesneau3103@gmail.com>
2020-12-03 18:15:16 +0100

    re commit classe

---
+++ projet final/action.js
@@ -0,0 +1,25 @@
+function Action(lanceur, cible, action_type)
+{
+    this.lanceur = lanceur;
+    this.cible = cible;
+
+    switch (action_type) {
+        case "attaque":
+            
+            break;
+        case "lancer sort":
+        
+            break;
+
+        case "defendre":
+        
+            break;
+    
+        case "fuir":
+    
+            break;
+
+        default:
+            break;
+    }
+}
--- projet final/personnage.js
+++ projet final/personnage.js
@@ -2,15 +2,55 @@ function personnage(name, classe, joueur, perso_type)
 {
     this.name = name;
     this.classe = classe;
-    switch (classe) {
+    if (perso_type) {
+        switch (classe) {
         case "guerrier":
-            this.hpmax = 100;
+
+            this.hpm = 100;
             this.hp = this.hpmax;
-            
+
+            this.mpm = 100;
+            this.mp = this.mpmax;
+
+            this.att = 100;
+            this.def = 100;
+
+            this.pui = 100;
+            this.mr = 100;
+
+            this.esq = 100;
+            this.vit = 100;
+
             break;
-    
         default:
             break;
+        }
+        this.argent = 0;
     }
+    else
+    {
+        switch (classe) {
+            case "guerrier":
     
+                this.hpm = 100;
+                this.hp = this.hpmax;
+    
+                this.mpm = 100;
+                this.mp = this.mpmax;
+    
+                this.att = 100;
+                this.def = 100;
+    
+                this.pui = 100;
+                this.mr = 100;
+    
+                this.esq = 100;
+                this.vit = 100;
+    
+                break;
+            default:
+                break;
+            }
+        this.argent = 30;
+    }
 }
